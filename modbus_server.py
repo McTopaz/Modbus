0ce345befa44fbb6bff1ce8d4cee3ebdac156cc2
Stefan Eriksson <stefan.k.j.eriksson@gmail.com>
2016-09-22 22:56:40 +0200

    Update

--- modbus_server.py
+++ modbus_server.py
@@ -34,7 +34,7 @@ def CreateRegisterValue(count, value):
 def DefineRegisters():
     ''''
     INT8: 0, 1, -1
-    UINT8: 1, 1, 0xAD
+    UINT8: 1, 1, 0xDE
     INT16: 2, 1, -1
     UINT16: 3, 1, 0xDEAD
     INT24: 4, 2, -1
@@ -57,57 +57,28 @@ def DefineRegisters():
     '''
     
     registers = {}
-    registers[0] = CreateRegisterValue(1, -1)
-    registers[1] = CreateRegisterValue(1, 0xAD)
-    registers[2] = CreateRegisterValue(1, 0xAD)
-    registers[3] = CreateRegisterValue(1, 0xAD)
-    registers[4] = CreateRegisterValue(2, 0xAD)
-    registers[6] = CreateRegisterValue(2, 0xAD)
-    registers[8] = CreateRegisterValue(2, 0xAD)
-    registers[10] = CreateRegisterValue(2, 0xAD)
-    registers[12] = CreateRegisterValue(3, 0xAD)
-    registers[15] = CreateRegisterValue(3, 0xAD)
-    registers[18] = CreateRegisterValue(4, 0xAD)
-    registers[22] = CreateRegisterValue(4, 0xAD)
-    registers[26] = CreateRegisterValue(2, 0xAD)
-    registers[28] = CreateRegisterValue(4, 0xAD)
-    registers[32] = CreateRegisterValue(1, 0xAD)
-    registers[33] = CreateRegisterValue(1, 0xAD)
-    registers[34] = CreateRegisterValue(2, 0xAD)
-    registers[36] = CreateRegisterValue(3, 0xAD)
-    registers[39] = CreateRegisterValue(4, 0xAD)
-    registers[43] = CreateRegisterValue(5, 0xAD)
-    registers[48] = CreateRegisterValue(6, 0xAD)
+    registers[0] = CreateRegisterValue(1, struct.pack(">h", -1))                    # INT8
+    registers[1] = CreateRegisterValue(1, struct.pack(">H", 0x00DE))                # UINT8
+    registers[2] = CreateRegisterValue(1, struct.pack(">h", -1))                    # INT16
+    registers[3] = CreateRegisterValue(1, struct.pack(">H", 0xDEAD))                # UINT16
+    registers[4] = CreateRegisterValue(2, struct.pack(">i", -1))                    # INT24
+    registers[6] = CreateRegisterValue(2, struct.pack(">I", 0x00DEADBE))            # UINT24
+    registers[8] = CreateRegisterValue(2, struct.pack(">i", -1))                    # INT32
+    registers[10] = CreateRegisterValue(2, struct.pack(">I", 0xDEADBEEF))           # UINT32
+    registers[12] = CreateRegisterValue(3, struct.pack(">q", -1))                   # INT48
+    registers[15] = CreateRegisterValue(3, struct.pack(">Q", 0x0000DEADBEEFBABE))   # UINT48
+    registers[18] = CreateRegisterValue(4, struct.pack("q", -1))                    # INT64
+    registers[22] = CreateRegisterValue(4, struct.pack(">Q", 0xDEADBEEFBABECAFE))   # UINT64
+    registers[26] = CreateRegisterValue(2, struct.pack("f", 0xDEADBEEF))            # REAL32
+    registers[28] = CreateRegisterValue(4, struct.pack("d", 0xDEADBEEFBABECAFE))    # REAL64
+    registers[32] = CreateRegisterValue(1, bytearray("A", 'ascii'))                 # STR1
+    registers[33] = CreateRegisterValue(1, bytearray("AB", 'ascii'))                # STR2
+    registers[34] = CreateRegisterValue(2, bytearray("ABCD", 'ascii'))              # STR4
+    registers[36] = CreateRegisterValue(3, bytearray("ABCDEF", 'ascii'))            # STR6
+    registers[39] = CreateRegisterValue(4, bytearray("ABCDEFGH", 'ascii'))          # STR8
+    registers[43] = CreateRegisterValue(5, bytearray("ABCDEFGHIJ", 'ascii'))        # STR10
+    registers[48] = CreateRegisterValue(6, bytearray("ABCDEFGHIJKL", 'ascii'))      # STR12
     return registers
-    
-    '''
-    data = struct.pack('bBhHiIiIqQqQfd',
-        -1,
-        0xAD,
-        -1,
-        0xDEAD,
-        -1,
-        0xDEADBE,
-        -1,
-        0xDEADBEEF,
-        -1,
-        0xDEADBEEDBABE,
-        -1,
-        0xDEADBEEFBABECAFE, 
-        0xDEADBEEF,
-        0x0EADBEEFBABECAFE)
-    
-    data += b"0A"           # strig[1]
-    data += b"AB"           # string[2]
-    data += b"0ABC"         # string[3]
-    data += b"ABCD"         # string[4]
-    data += b"ABCDEF"       # string[6]
-    data += b"ABCDEFGH"     # string[8]
-    data += b"ABCDEFGHIJ"   # string[10]
-    data += b"ABCDEFGHIJKL" # string[12]
-    
-    return data
-    '''
 
 def PrintData(data):
     tstr = ""
@@ -159,7 +130,7 @@ class ModbusRequest():
         self.count = struct.unpack('>h',  data[4:6])[0]
         self.valid = False
         
-    def ValidRequest(self):
+    def ValidateRequest(self):
         # Overload in sub class.
         pass
         
@@ -167,20 +138,13 @@ class ModbusRequest():
         start = self.start
         count = self.count
         return start in registers and count in registers[start]
-
-        '''
-        end = (self.start + self.count) * 2
-        print("end: %s"%(end))
-        print("len: %s"%(len(registers)))
-        return end <= len(registers) and end != -1
-        '''
         
 class ModbusRtuRequest(ModbusRequest):
     def __init__(self,  data):
         ModbusRequest.__init__(self, data, data[0:6])
         self.crc = struct.unpack('h', data[6:8])[0]
         
-    def ValidRequest(self):
+    def ValidateRequest(self):
         PrintData(self.request)
         calcedCrc = self.CalculateCRC(self.request[0:6])
         okLength = len(self.request) == 8
@@ -205,7 +169,7 @@ class ModbusTcpRequest(ModbusRequest):
     def __init__(self,  data):
         ModbusRequest.__init__(self, data, data[6:12])
     
-    def ValidRequest(self):
+    def ValidateRequest(self):
         okLength = len(self.request) == (6 + self.count)
         okSlaveAddress = self.slaveAddress == slaveAddress
         okFunction = self.function == 0x03 or self.function == 0x04
@@ -214,48 +178,77 @@ class ModbusTcpRequest(ModbusRequest):
 #=== Modbus response ==========================================================
 
 class ModbusResponse():
-    def __init__(self):
-        pass
+    def __init__(self,  slaveAddress, function):
+        self.slaveAddress = slaveAddress
+        self.function = function
 
 class ModbusRtuResponse(ModbusResponse):
-    def __init__(self):
-        pass
+    def __init__(self,  slaveAddress, function):
+        ModbusResponse.__init__(self, slaveAddress, function)
+        
+    def CreatePositiveResponse(self, data):
+        response = struct.pack("BBB", self.slaveAddress, self.function, len(data))
+        response += data 
+        crc = self.CalculateCRC(response)
+        response += struct.pack("H", crc)
+        return response
+        
+    def CreateNegativeResponse(self, error):
+        response = struct.pack("BBB", self.slaveAddress, self.function, error)
+        crc = self.CalculateCRC(response)
+        response += struct.pack("H", crc)
+        return response
+        
+    def CalculateCRC(self,  data):
+        crc = 0xFFFF
+        for pos in data:
+            crc ^= pos 
+            for i in range(8):
+                if ((crc & 1) != 0):
+                    crc >>= 1
+                    crc ^= 0xA001
+                else:
+                    crc >>= 1
+        return crc
         
 class ModbusTcpResponse(ModbusResponse):
-    def __init__(self):
-        pass
-
+    def __init__(self,  slaveAddress, function):
+        ModbusResponse.__init__(self, slaveAddress, function)
+        
+    def CreatePositiveResponse(self, data):
+        return b""
+        
+    def CreateNegativeResponse(self, error):
+        return b""
+        
 #=== Modbus server ============================================================
         
 class ModbusServer():
     def __init__(self):
         self.request = 0
-        self.response  = 0
+        self.response = 0
         
     def ParseRequest(self,  request):
         # Overload in sub class.
         pass
         
     def AcceptableRequest(self):
-        self.request.ValidRequest()
+        self.request.ValidateRequest()
         print("Request is valid: %s"%(self.request.valid))
         defined = self.request.IsRegisterDefined()
-        print("Register is defined: %s"%(defined)
+        print("Register is defined: %s"%(defined))
         return self.request.valid and defined
       
     def CreateResponse(self):
         if (self.AcceptableRequest):
-            return self.CreatePositiveResponse()
+            print("Start: %s"%self.request.start)
+            print("Count: %s"%self.request.count)
+            data = registers[self.request.start][self.request.count]
+            PrintData(data)
+            return self.response.CreatePositiveResponse(data)
         else:
-            return self.CreateNegativeResponse()
-            
-    def CreatePositiveResponse(self):
-        # Overload in sub class.
-        pass
-        
-    def CreateNegativeResponse(self):
-        # Overload in sub class.
-        pass
+            error = 0
+            return self.response.CreateNegativeResponse(error)
 
 class ModbusRtuServer(ModbusServer):
     def __init__(self,):
@@ -263,12 +256,7 @@ class ModbusRtuServer(ModbusServer):
         
     def ParseRequest(self,  request):
         self.request = ModbusRtuRequest(request)
-        
-    def CreatePositiveResponse(self):
-        return b""
-        
-    def CreateNegativeResponse(self):
-        return b""
+        self.response = ModbusRtuResponse(self.request.slaveAddress, self.request.function)
         
 class ModbusTcpServer(ModbusServer):
     def __init___(self):
@@ -276,12 +264,7 @@ class ModbusTcpServer(ModbusServer):
 
     def ParseRequest(self,  request):
         self.request = ModbusTcpRequest(request)
-        
-    def CreatePositiveResponse(self):
-        return b""
-        
-    def CreateNegativeResponse(self):
-        return b""
+        self.response = ModbusTcpResponse(self.request.slaveAddress, self.request.function)
 
 #=== Main =====================================================================
 
